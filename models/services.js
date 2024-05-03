003bdb2a86e4ee4b4e17e10710539b6ec67ba91d
BeastPT <ajfa2004@gmail.com>
2024-05-03 16:58:51 +0100

    resumo

---
+++ models/services.js
@@ -0,0 +1,10 @@
+/*
+ - type ( /!\ VERIFICAR )
+ - clientid
+ - state (Ativo / Finalizado / Cancelado)
+ - rating ( 1-5 estrelas / null )
+ - data (criacao)
+ - price 
+
+
+*/
---
+++ models/user.js
@@ -0,0 +1,20 @@
+/*
+ - username
+ - email
+ - password
+
+ - Nome Completo
+ - Address
+ - Telemovel
+ - Código Postal
+ - País
+ - Cidade
+ - Porta
+ - Nacionalidade
+ - Data de Nascimento
+
+ - data (criacao)
+
+ /!\ VERIFICAR
+
+*/
